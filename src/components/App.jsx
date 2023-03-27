bd7b09b86ce1c7a53a14c117c4e61e365ecbc901
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-03-27 19:32:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,153 +1,82 @@
-import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
-import {Error} from '../components/TextSearchError/TextSearchError';
-import * as API from './../servises/API';
-
-import {Searchbar} from './Searchbar/Searchbar';
+import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
-import {Loader} from './Loader/Loader';
-import {Button} from './Button/Button';
-import {Modal} from './Modal/Modal';
-import {Container} from './app.styled';
+import fetchImagesWithQuery from 'services/api';
+import Modal from './Modal/Modal';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+import s from './App.module.css';
+import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+export default function App() {
+  const [searchData, setSearchData] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(0);
+  const [largeImage, setLargeImage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [error, setError] = useState(null);
 
-class App extends Component{
-state={
-  query: '',
-    page: 1,
-    images: [],
-    showModal: false,
-    largeImageURL: '',
-    tags: '',
-    showBtn: false,
-    isLoading: false,
-    error: false,
-    status: 'idle',
+  useEffect(() => {
+    if (!page) {
+      return;
+    }
 
-}
-
-
-componentDidMount() {
-  document.addEventListener('keydown', this.onKeyEscPress, false);
-}
-componentDidUpdate(prevProps, prevState) {
-  const { images, query, page } = this.state;
-  if (query !== prevState.query || page !== prevState.page) {
-    this.setState({
-      isLoading: true,
-    });
-  }
-
-  if (
-    query !== prevState.query ||
-    page !== prevState.page ||
-    !images.length
-  ) {
-    API.getImages(query, page)
-      .then(data => {
-        if (!data.hits.length) {
-          this.setState({
-            showBtn: false,
-          });
-          return;
-        }
-        console.log('data =', data);
-        if (!images.length) {
-          this.setState(prevState => ({
-            images: [...data.hits],
-            showBtn: true,
-          }));
-        } else
-          this.setState(prevState => ({
-            images: [...prevState.images, ...data.hits],
-            showBtn: true,
-          }));
-        if (data.totalHits <= this.state.images.length + 12)
-          this.setState({
-            showBtn: false,
-          });
-      })
-      .catch(error => {
-        this.setState({
-          error: true,
-          })
-          return;
-  
-      })
-      .finally(
-        setTimeout(() => {
-          this.setState({ isLoading: false });
-        }, 1000)
-      );
-  }
-}
-componentWillUnmount() {
-  document.removeEventListener('keydown', this.onKeyEscPress, false);
-}
-  onSubmitForm = word => {
-    this.setState({
-      query: word,
-      images: [],
-      page: 1,
-    });
-  };
-
-  onClickButton = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-
-  };
-
-  onClickClose = () => {
-    this.setState({
-      showModal: false,
-    });
-  };
-
-  onClickImage = ({ largeImageURL, tags }) => {
-    this.setState({
-      showModal: true,
-      largeImageURL: largeImageURL,
-      tags: tags,
-    });
-  };
-
-  onKeyEscPress = event => {
-    if (event.keyCode === 27)
-      this.setState({
-        showModal: false,
+    try {
+      setIsLoading(true);
+      const response = fetchImagesWithQuery(searchData, page);
+      response.then(data => {
+        data.data.hits.length === 0
+          ? toast.error('Nothing found')
+          : data.data.hits.forEach(({ id, webformatURL, largeImageURL }) => {
+              !images.some(image => image.id === id) &&
+                setImages(i => [...i, { id, webformatURL, largeImageURL }]);
+            });
+        setIsLoading(false);
       });
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, searchData]);
+
+  const onSubmit = newSearchData => {
+    if (newSearchData.trim() === '') {
+      return toast.error('Enter the meaning for search');
+    } else if (newSearchData === searchData) {
+      return;
+    }
+    setSearchData(newSearchData);
+    setPage(1);
+    setImages([]);
   };
 
-  render() {
-    const {isLoading, showBtn, showModal, images, largeImageURL, tags, error} = this.state;
-    
-    return(
-      <Container>
-        <ToastContainer 
-        position="top-right"
-        toastOptions={{
-          duration: 2000,
-        }}/>
-        <Searchbar onSubmitForm={this.onSubmitForm} />
-        <ImageGallery
-          images={images}
-          onClickImage={this.onClickImage}
-        />
-        {isLoading && <Loader />}
-        {showBtn && <Button onClickButton={this.onClickButton} />}
-        {showModal && (
-          <Modal
-            image={largeImageURL}
-            tags={tags}
-            onClickClose={this.onClickClose}
-          />
-        )}
-        {error && <Error />}
-      </Container>
-    )
-  }
-}
+  const nextPage = () => {
+    setPage(p => p + 1);
+  };
 
-export default App
+  const openModal = index => {
+    setShowModal(true);
+    setLargeImage(images[index].largeImageURL);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  return (
+    <div className={s.App}>
+      <Searchbar onSubmit={onSubmit} />
+      {images.length !== 0 && (
+        <ImageGallery images={images} openModal={openModal} />
+      )}
+      {showModal && <Modal toggleModal={toggleModal} largeImage={largeImage} />}
+      {isLoading && <Loader />}
+      <ToastContainer autoClose={2500} />
+      {images.length >= 12 && <Button nextPage={nextPage} />}
+    </div>
+  );
+}

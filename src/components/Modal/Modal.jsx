21e1415b565e458b5bb41c7874e58abf2adbdefb
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-03-27 19:33:18 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,18 +1,31 @@
+import { useEffect } from 'react';
+import s from './Modal.module.css';
 import PropTypes from 'prop-types';
-import { Overlay, ModalWindow } from './Modal.styled';
 
-export const Modal = ({ image, tags, onClickClose }) => {
+export default function Modal({ toggleModal, largeImage }) {
+  useEffect(() => {
+    const handleKeyDown = e => e.code === 'Escape' && toggleModal();
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [toggleModal]);
+
+  const handleBackdropClick = e => {
+    e.target === e.currentTarget && toggleModal();
+  };
+
   return (
-    <Overlay onClick={onClickClose}>
-      <ModalWindow>
-        <img src={image} alt={tags} />
-      </ModalWindow>
-    </Overlay>
+    <div className={s.Overlay} onClick={handleBackdropClick}>
+      <div className={s.Modal}>
+        <img src={largeImage} alt="" />
+      </div>
+    </div>
   );
-};
+}
 
 Modal.propTypes = {
-  image: PropTypes.string.isRequired,
-  tags: PropTypes.string.isRequired,
-  onClickClose: PropTypes.func.isRequired,
+  toggleModal: PropTypes.func.isRequired,
+  largeImage: PropTypes.string.isRequired,
 };
